300cab06a4e2ec7e7611e0b3041094644d4e10cb
tsurowa <127434543+tsurowa@users.noreply.github.com>
2023-03-09 21:28:20 +0200

    Create 01.js

---
+++ Objects Lab/01.js
@@ -0,0 +1,11 @@
+function solve(name, population, treasury) {
+  myObj = {
+    name,
+    population,
+    treasury,
+  };
+  return myObj;
+}
+
+console.log(solve("Tortuga", 7000, 15000));
+console.log(solve("Santo Domingo", 12000, 23500));
