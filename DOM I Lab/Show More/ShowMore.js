042e6b9dde2a3bb9e7d6b9ef000b07fb1be9ee24
tsurowa <127434543+tsurowa@users.noreply.github.com>
2023-03-09 20:40:17 +0200

    Create ShowMore.js

---
+++ DOM I Lab/Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let textElement = document.getElementById('text');
+    textElement.style.display = 'inline';
+    let hiddenElement = document.getElementById('more');
+    hiddenElement.style.display = 'none';
+
+}
