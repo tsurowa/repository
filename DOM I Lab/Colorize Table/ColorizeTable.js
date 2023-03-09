20d75ee6727d4d9430481c5b3405fe3c44d9408c
tsurowa <127434543+tsurowa@users.noreply.github.com>
2023-03-09 20:41:02 +0200

    Create ColorizeTable.js

---
+++ DOM I Lab/Colorize Table/ColorizeTable.js
@@ -0,0 +1,9 @@
+function colorize() {
+    let rowElements = document.getElementsByTagName('tr');
+    let rows = Array.from(rowElements);
+    rows.forEach((x, i) => {
+        if(i % 2 != 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    });
+}
