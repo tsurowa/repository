3e84f97ac61ce9bbb8fccfb7f9a5a7eda43ff99e
tsurowa <127434543+tsurowa@users.noreply.github.com>
2023-03-09 20:25:37 +0200

    Create 01.js

---
+++ Arrays Exercise/01.js
@@ -0,0 +1,9 @@
+function PrintArrayWithDelimiter(arr, delimeter) {
+    console.log(arr.join(delimeter));
+}
+
+PrintArrayWithDelimiter(["One", "Two", "Three", "Four", "Five"], "-");
+PrintArrayWithDelimiter(
+  ["How about no?", "I", "will", "not", "do", "it!"],
+  "_"
+);
