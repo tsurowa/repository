dc5d1e3e45ed36d478e47008dacf62655b2e90db
tsurowa <127434543+tsurowa@users.noreply.github.com>
2023-03-09 20:16:16 +0200

    Create 01.js

---
+++ Advanced functions Lab/01.js
@@ -0,0 +1,23 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+function solve(area, vol, input) {
+    let coordinates = JSON.parse(input);
+    let result = [];
+    for (let line of coordinates) {
+        result.push({
+            area: area.call(line),
+            volume: vol.call(line)
+        });
+    }
+    return result;
+}
+
+console.log(solve(area, vol, `[
+{"x":"1","y":"2","z":"10"},
+{"x":"7","y":"7","z":"10"},
+{"x":"5","y":"2","z":"10"}
+]`));
